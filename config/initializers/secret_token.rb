40023f14988b5e8557cce3e7c2405986166c0281
Alex Hsi <alexhhsi@gmail.com>
2011-06-05 13:39:53 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Milkmade::Application.config.secret_token = 'a7bea774ba6bd96de48329fcde255993f39642ebf428a410a500a6553afa89409a6620a51bff4b9e0cd90715773f19571f7c0eea60c6a835561cf68a35cda74d'
